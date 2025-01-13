27de145894024f0fc3139a40ffe96b7e7cc7a169
emersonTH1 <hanccotomayllaemerson@gmail.com>
2025-01-13 06:51:51 -0500

    avanze

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -7,7 +7,31 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Avatar Page"),
+        title: const Text(
+          "Avatar Page",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.pexels.com/photos/27893231/pexels-photo-27893231/free-photo-of-puesta-de-sol-moda-fotografo-fotografia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Image.network(
+              "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+        ],
       ),
     );
   }
